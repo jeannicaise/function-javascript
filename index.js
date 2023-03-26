8b1b11e75fdda0c4fc9ea77009191f5dc2c26b7f
Jean-Nicaise <jeannicaise2468@gmail.com>
2023-03-26 13:23:34 +0200

    ajout de fonction

---
+++ index.js
@@ -0,0 +1,24 @@
+
+
+// Demande à l'utilisateur de saisir un mot de passe ;
+// compare ce dernier à une chaîne de caractères choisie (par exemple "secret").
+// Si les mots de passe correspondent: affiche "Welcome! 👋" dans la console ;
+// Sinon, affiche "Wrong password! ❌".
+
+//je recupere tout ce que l'utilisateur va taper dans une variable
+// const userName = prompt("Veuillez entrer ton nom")
+
+
+// Déclare et définis trois variables à l'aide d'une valeur numérique.
+// (ex : const a = 1)
+
+// Si les trois variables sont égales, affiche "Les trois variables sont identiques" dans la console
+// Si seulement deux des variables sont égales, affiche "Deux des variables sont identiques" dans la console
+// Si les variables sont toutes différentes, affiche "toutes les variables sont différentes" dans la console
+
+const a = 1;
+const b = 2;
+const c = 2;
+if (a ===b && a === c && b === c) {onsole.log("Les trois variables sont identiques");} 
+else if (a ===b || a === c ||b === c){console.log("Deux des variables sont dentiques");}
+else {console.log("toutes les variables sont différentes");}
